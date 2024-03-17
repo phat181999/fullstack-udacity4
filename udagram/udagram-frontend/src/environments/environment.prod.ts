92dea63a931eda3e706eb03610922081c42f1d4e
phat181999 <60052476+phat181999@users.noreply.github.com>
2024-03-17 22:41:22 +0700

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
