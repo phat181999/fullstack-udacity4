a32f471bc761d0dcdbc548d620bbd4db32b29ab3
phat181999 <60052476+phat181999@users.noreply.github.com>
2024-03-17 22:48:48 +0700

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'Udagram-ebs-env.eba-kahzmdec.us-east-1.elasticbeanstalk.com',
 };
 
 /*
