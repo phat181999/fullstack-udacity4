b501d3ddc2323e1551d0590f2f58cc63b239f2f7
phat181999 <60052476+phat181999@users.noreply.github.com>
2024-03-19 15:58:01 +0700

    Update config.ts

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,20 +1,22 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  username: "databasepgadmin",
+  password: "databasepgadmin",
+  database: "databasepgadmin",
+  host: "databasepgadmin.cxzddeylzo0t.us-east-1.rds.amazonaws.com",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  url: "http://localhost:8080",
   jwt: {
     secret: process.env.JWT_SECRET,
   },
